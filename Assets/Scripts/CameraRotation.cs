665c145fdf6675838c378c751307b1c4ad661d1d
David Santos <sandokas@gmail.com>
2016-12-10 12:52:11 +0000

    My skillz suck

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+    public float speed = 2;
+    Vector3 forward;
+    
+    public Transform playerTransform;
+	// Use this for initialization
+	void Start () {
+        //playerTransform = GetComponentInParent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Camera camera = GetComponent<Camera>();
+        if (Input.GetMouseButton(1))
+        {
+            //Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            Vector3 mousePosition = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.nearClipPlane));
+            forward = mousePosition - camera.transform.position;
+        }
+        
+
+    }
+    void LateUpdate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            //Vector3 playerForward = Vector3.Cross(forward, new Vector3(1, 0, 1));
+            //Quaternion cameraRotation = Quaternion.LookRotation(forward.normalized, Vector3.up);
+            Quaternion playerRotation = Quaternion.LookRotation(forward.normalized, Vector3.up);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, cameraRotation, Time.deltaTime * speed);
+            playerTransform.rotation = Quaternion.Slerp(transform.rotation, playerRotation, Time.deltaTime * speed);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 5;
+
+    public bool IsPlayerMoving
+    {
+        get { return isPlayerMoving; }
+    }
+
+    private Rigidbody rb;
+    private bool isPlayerMoving = false;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isPlayerMoving = false;
+        Vector3 direction = Vector3.zero;
+	    if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            isPlayerMoving = true;
+            direction = direction + transform.forward;
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            //transform.Rotate
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            //rb.AddRelativeForce(Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+            isPlayerMoving = true;
+            direction = direction -transform.forward;
+        }
+        rb.AddRelativeForce(direction.normalized * speed * Time.deltaTime,ForceMode.Impulse);
+    }
+}
