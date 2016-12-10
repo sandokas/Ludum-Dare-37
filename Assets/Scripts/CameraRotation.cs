a9d714009968a309be7a360139feaaf50061223c
David Santos <sandokas@gmail.com>
2016-12-10 14:07:22 +0000

    breaking the players leg
    breaking the players leg

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -5,7 +5,6 @@ public class CameraRotation : MonoBehaviour {
     public float speed = 2;
     Vector3 forward;
     
-    public Transform playerTransform;
 	// Use this for initialization
 	void Start () {
         //playerTransform = GetComponentInParent<Transform>();
@@ -16,22 +15,22 @@ public class CameraRotation : MonoBehaviour {
         Camera camera = GetComponent<Camera>();
         if (Input.GetMouseButton(1))
         {
-            //Ray ray = camera.ScreenPointToRay(Input.mousePosition);
             Vector3 mousePosition = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.nearClipPlane));
+            Vector3 eulerAngles = transform.rotation.eulerAngles;
+            if (eulerAngles.y < 90)
+                mousePosition.x = 90;
+            if (eulerAngles.y > 270)
+                mousePosition.x = -90;
             forward = mousePosition - camera.transform.position;
         }
-        
 
-    }
-    void LateUpdate()
+        }
+        void LateUpdate()
     {
         if (Input.GetMouseButton(1))
         {
-            //Vector3 playerForward = Vector3.Cross(forward, new Vector3(1, 0, 1));
-            //Quaternion cameraRotation = Quaternion.LookRotation(forward.normalized, Vector3.up);
-            Quaternion playerRotation = Quaternion.LookRotation(forward.normalized, Vector3.up);
-            //transform.rotation = Quaternion.Slerp(transform.rotation, cameraRotation, Time.deltaTime * speed);
-            playerTransform.rotation = Quaternion.Slerp(transform.rotation, playerRotation, Time.deltaTime * speed);
+            Quaternion cameraRotation = Quaternion.LookRotation(forward.normalized, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, cameraRotation, Time.deltaTime * speed);
         }
     }
 }
